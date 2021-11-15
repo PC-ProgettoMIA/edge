c55ebf5d097940b46602c1e838b46ab6d80b4a08
Matteo Scucchia <matteo.scucchia@studio.unibo.it>
2021-11-15 15:44:39 +0100

    dt structured with eclipse ditto meta model

--- src/controller.js
+++ src/controller.js
@@ -2,9 +2,12 @@ const dt_instance = require("./iot/dt_instance");
 const dt = dt_instance.instance;
 
 exports.getProperty = (name) => async (req, res) => {
-    let property = await dt.get(name)
+    let feature = await dt.get("features")
+    let property = feature[name]
     if (property) {
-        return res.status(200).json({ name: property });
+        let response = {}
+        response[name] = property.properties.value
+        return res.status(200).json(response);
     }
     return res.status(400).send("The digital twin has no property '" + name + "'")
 };
--- src/iot/dt_instance.js
+++ src/iot/dt_instance.js
@@ -17,21 +17,35 @@ setInterval(async function () {
 //id and gps set in the initialization
 //TODO uniform to eclipse ditto model
 async function initialize() {
-    let id = await dt.get("id")
+    let id = await dt.get("thingId")
 
     //set the id the first time
     if (!id) {
         let id = namespace + ":" + uuidv4()
-        await dt.set("id", id)
+        await dt.set("thingId", id)
     }
 
-    let gps = await dt.get("gps")
+    let attributes = await dt.get("attributes")
 
-    //set the gps the first time
-    if (!gps) {
-        gps = setGPS()
-        await dt.set("gps", gps)
+    if (!attributes) {
+        let gps = setGPS()
+        let att = {
+            gps: gps
+        }
+        await dt.set("attributes", att)
     }
+
+    await dt.set("features", {
+        temp: { properties: {} },
+        hum: { properties: {} },
+        press: { properties: {} },
+        co2TVOC: { properties: {} },
+        qual: { properties: {} },
+        wind: { properties: {} },
+        uv: { properties: {} },
+        rain: { properties: {} }
+    })
+
 }
 
 //TODO add exception for gps not present?
@@ -55,14 +69,122 @@ async function updateSensorValues() {
     let wind = sensors.wind()
     let uv = sensors.uv()
     let rain = sensors.rain()
-    await dt.set("temp", temperature)
-    await dt.set("hum", humidity)
-    await dt.set("press", pressure)
-    await dt.set("co2TVOC", co2)
-    await dt.set("qual", quality)
-    await dt.set("wind", wind)
-    await dt.set("uv", uv)
-    await dt.set("rain", rain)
+
+    let time = Date.now()
+
+    let features = await dt.get("features")
+
+    if (temperature != null) { //if sensor broken
+        let props = {
+            connected: true,
+            timestamp: time,
+            value: temperature
+        }
+        features.temp.properties = props
+    }
+    else {
+        features.temp.properties.connected = false
+    }
+
+    if (humidity != null) { //if sensor broken
+        let props = {
+            connected: true,
+            timestamp: time,
+            value: humidity
+        }
+        features.hum.properties = props
+    }
+    else {
+        features.hum.properties.connected = false
+    }
+
+    if (pressure != null) { //if sensor broken
+        let props = {
+            connected: true,
+            timestamp: time,
+            value: pressure
+        }
+        features.press.properties = props
+    }
+    else {
+        features.press.properties.connected = false
+    }
+
+    if (co2 != null) { //if sensor broken
+        let props = {
+            connected: true,
+            timestamp: time,
+            value: co2
+        }
+        features.co2TVOC.properties = props
+    }
+    else {
+        features.co2TVOC.properties.connected = false
+    }
+
+    if (quality != null) { //if sensor broken
+        let props = {
+            connected: true,
+            timestamp: time,
+            value: quality
+        }
+        features.qual.properties = props
+    }
+    else {
+        features.qual.properties.connected = false
+    }
+
+    if (co2 != null) { //if sensor broken
+        let props = {
+            connected: true,
+            timestamp: time,
+            value: co2
+        }
+        features.co2TVOC.properties = props
+    }
+    else {
+        features.co2TVOC.properties.connected = false
+    }
+
+    if (wind != null) { //if sensor broken
+        let props = {
+            connected: true,
+            timestamp: time,
+            value: wind
+        }
+        features.wind.properties = props
+    }
+    else {
+        features.wind.properties.connected = false
+    }
+
+    if (uv != null) { //if sensor broken
+        let props = {
+            connected: true,
+            timestamp: time,
+            value: uv
+        }
+        features.uv.properties = props
+    }
+    else {
+        features.uv.properties.connected = false
+    }
+
+    if (rain != null) { //if sensor broken
+        let props = {
+            connected: true,
+            timestamp: time,
+            value: rain
+        }
+        features.rain.properties = props
+    }
+    else {
+        features.rain.properties.connected = false
+    }
+
+    await dt.set("features", features)
+
+
 
 }
 
